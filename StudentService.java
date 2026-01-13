7305b757905d702121e7ad2fbfb17919157885bc
SASIVARUN-07 <sasiff510@gmail.com>
2026-01-13 21:22:51 +0530

    Update StudentService.java

--- StudentService.java
+++ StudentService.java
@@ -14,6 +14,17 @@ public class StudentService {
         }
         System.out.println("Student with ID " + id + " not found.");
     }
+    void updateStudent(int id, String newName, String newBranch) {
+        for (Student s : list) {
+            if (s.id == id) {
+                s.name = newName;
+                s.branch = newBranch;
+                System.out.println("Student with ID " + id + " updated.");
+                return;
+            }
+        }
+        System.out.println("Student with ID " + id + " not found.");
+    }
     void deletestudent(int id) {
         Iterator<Student> it = list.iterator();
         while (it.hasNext()) {
